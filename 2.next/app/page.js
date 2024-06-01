0067dd46ea27fccfa5b9eea33de98bb54712d6d0
kevingida <kevingida@gmail.com>
2024-06-01 15:15:33 +0200

    initiate next project

---
+++ 2.next/app/page.js
@@ -0,0 +1,5 @@
+import BmwE24 from "@/components/bmwE24/page";
+
+export default function Home() {
+  return <BmwE24 />;
+}
---
+++ 2.next/components/bmwE24/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BmwE24 = () => {
+  return <div>BmwE24</div>;
+};
+
+export default BmwE24;
